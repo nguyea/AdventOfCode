7982a85781e6f310f07870e9f1d277fc53f342e4
nguyea <18NguyeA@fulford.york.sch.uk>
2023-12-01 10:21:01 +0000

    new

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+            string[] lines = File.ReadAllLines("input")
         }
     }
 }
